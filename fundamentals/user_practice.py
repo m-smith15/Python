eab6682069e8a4d1612a677374c2011613448e34
m-smith15 <mpsmith1215@gmail.com>
2022-07-06 17:51:51 -0400

    chaining practice commit

--- fundamentals/user_practice.py
+++ fundamentals/user_practice.py
@@ -14,6 +14,7 @@ class createUser:
         print(self.age)
         print(self.is_rewards_member)
         print(self.gold_card_points)
+        return self
     
     def enroll(self):
         if self.is_rewards_member == False:
@@ -22,13 +23,16 @@ class createUser:
             print(f"It is {self.is_rewards_member}, you are a member with {self.gold_card_points} in your balance")
         else:
             print("User is already a member")
+        return self
 
     def spend_points(self, amount):
         if self.gold_card_points < amount:
             print("What do you think this is, Enron? -- not enough points in balance")
+            
         else:
             self.gold_card_points -= amount
             print(f"You have {self.gold_card_points} gold card points remaining")
+        return self
 
 # Create user Timmy Shrimp, email timmytheshrimp@ocean.gov, age 1050
 print("------1--------")
@@ -52,22 +56,25 @@ user1.spend_points(200) #should only have balance of 100, so should see error me
 print("-------------Assignment Parameters ------------")
 
 # --------- Assignment Parameters ---------
-assignment_user1 = createUser("Tester","One","testerone@email.edu",50)
-assignment_user1.display_info()
-assignment_user1.enroll()
-
-assignment_user2 = createUser("Nottester","Two","notatest@email.edu",40)
+# --------- Updating for chaining assignment --------
+print("----Assignment user 1----")
+assignment_user1 = createUser("Tester","One","testerone@email.edu",50).display_info().enroll().spend_points(50).display_info().enroll()
+# assignment_user1.display_info()
+# assignment_user1.enroll()
+print("----Assignment user 2----")
+assignment_user2 = createUser("Nottester","Two","notatest@email.edu",40).enroll().spend_points(80).display_info()
 # assignment_user2.display_info() 
-assignment_user3 = createUser("Yepitsatest","Three","jkistest@email.edu",30)
+print("----Assignment user 3----")
+assignment_user3 = createUser("Yepitsatest","Three","jkistest@email.edu",30).display_info().spend_points(40)
 # assignment_user3.display_info()
 
-assignment_user1.spend_points(50)
-assignment_user2.enroll()
-assignment_user2.spend_points(80)
-assignment_user1.display_info() #
-assignment_user2.display_info()
-assignment_user3.display_info()
+# assignment_user1.spend_points(50)
+# assignment_user2.enroll()
+# assignment_user2.spend_points(80)
+# assignment_user1.display_info() 
+# assignment_user2.display_info()
+# assignment_user3.display_info()
 
-assignment_user1.enroll() #bonus 1
+# assignment_user1.enroll() #bonus 1
 
-assignment_user3.spend_points(40) # bonus 2
+# assignment_user3.spend_points(40) # bonus 2
