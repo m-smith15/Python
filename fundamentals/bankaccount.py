02fccbcd289da25f0ed7e227b7de980e5b718279
m-smith15 <mpsmith1215@gmail.com>
2022-07-06 22:22:08 -0400

    bankaccount update commit

--- fundamentals/bankaccount.py
+++ fundamentals/bankaccount.py
@@ -2,9 +2,10 @@ class BankAccount:
     #declaring class attribute, has to be outside init
     all_accounts = []
 
-    def __init__(self, int_rate, balance): 
+    def __init__(self, int_rate, balance, accountname): 
         self.int_rate = int_rate
         self.balance = balance
+        self.accountname = accountname
         #need to add to all_account list for class function
         BankAccount.all_accounts.append(self)
 
@@ -22,7 +23,7 @@ class BankAccount:
         return self
 
     def display_account_info(self):
-        print(f"Balance is {self.balance}")
+        print(f"Balance is {self.balance} for account {self.accountname}")
         return self
 
     def yield_interest(self):
@@ -35,8 +36,47 @@ class BankAccount:
         for account in cls.all_accounts:
             print(account.balance)
         return cls
+#creating user class
+class User:
+    
+    user_accounts = []
 
-user1 = BankAccount(0.01,0).deposit(10).deposit(20).deposit(30).withdraw(50).yield_interest() #expecting 10.1 to display, 60 in dep 50 withdrawn 10 * .01 interest
-user2 = BankAccount(.01, 0).deposit(20).deposit(20).withdraw(5).withdraw(5).withdraw(5).withdraw(5).yield_interest() #expecting to see 20.2, 40 in deposits, 20 in withdrawl 0.01 interest
-#challenge - use a class to display all account info
-BankAccount.account_balance()
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def user_createAccount(self,int_rate, balance, accountname):
+        self.account = BankAccount(int_rate, balance, accountname) #pass in accountname when creating account, then append to list - can use to loop through accounts in classmethod below.
+        # User.user_accounts.append(self) #getting error here - why? Same as BankAccount?
+        return self
+        
+
+    def user_deposit(self, amount): #user CLASS instance reaching out to bankaccount CLASS' deposit method and depositing amount
+        self.account.deposit(amount)
+        return self
+    
+    def user_withdrawl(self, amount):
+        self.account.withdraw(amount)
+        return self
+    
+    def user_display_info(self):
+        self.account.display_account_info()
+        return self
+
+    @classmethod
+    def user_accounts(cls):
+        for account in cls.user_accounts:
+            print(account.accountname)
+        return cls
+
+# user1 = BankAccount(0.01,0).deposit(10).deposit(20).deposit(30).withdraw(50).yield_interest() #expecting 10.1 to display, 60 in dep 50 withdrawn 10 * .01 interest
+# user2 = BankAccount(.01, 0).deposit(20).deposit(20).withdraw(5).withdraw(5).withdraw(5).withdraw(5).yield_interest() #expecting to see 20.2, 40 in deposits, 20 in withdrawl 0.01 interest
+# #challenge - use a class to display all account info
+# BankAccount.account_balance()
+
+user_one = User("Example", "lobster@ocean.gov")
+user_one.user_createAccount(0.01, 0, "Fishing").user_display_info()
+#.user_createAccount("Shrimping").user_display_info()
+#User.user_accounts()
+user_two = User("Test","shrimp@ocean.gov")
+#user_one.user_display_info()
