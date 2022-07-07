11f21c120df5211e9dfc939a7f93f039e781c845
m-smith15 <mpsmith1215@gmail.com>
2022-07-07 19:14:59 -0400

    updating/adding static folders

--- flask/hello_flask/server.py
+++ flask/hello_flask/server.py
@@ -1,21 +1,21 @@
-from flask import Flask #improting Flask module
+from flask import Flask, render_template #improting Flask module
 # import render
 app = Flask(__name__) #creating a new instance of Flask called variable app
 
 @app.route('/') #the landing page of the route. The URL doesn't need to have the / in it (ex localhost:5000)
 def hello_world():
-    return "Hello World!"
+    return render_template("index.html", phrase="hello", times=5)
 
 @app.route('/dojo')
 def hello_dojo():
     return "Hello Dojo!"
 
-@app.route('/say/<name>')
+@app.route('/say/<string:name>')
 def hello_input(name):
     name = name.capitalize()
     return f"Hello {name}!!"
 
-@app.route('/repeat/<int:x>/<repeat>')
+@app.route('/repeat/<int:x>/<string:repeat>')
 def repeat_input(x, repeat):
     #type of x int
     #return render of html file
