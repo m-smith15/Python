453cbb0e4614ff13f072aadf40d17127a2b42a63
m-smith15 <mpsmith1215@gmail.com>
2022-07-09 14:55:07 -0400

    updating for bonus obj

--- flask/counter/server.py
+++ flask/counter/server.py
@@ -3,32 +3,40 @@ from flask import Flask, render_template, session, request, redirect #remember s
 app = Flask(__name__)
 app.secret_key = "example secret key"
 
-@app.route('/')
+@app.route("/")
 def landing_page():
     print("landing page")
     if "countPy" not in session:
         session["countPy"] = 0
+    if "visits" not in session:
+        session["visits"] = 1
+    else:
         session["visits"] += 1
     return render_template("index.html")
 
-@app.route('/click')
+@app.route("/click")
 def clicked_button():
     print("button clicked")
     session["countPy"] += 1
-    return render_template("index.html")
+    return redirect("/")
 
-@app.route('/click/2')
-def clicked_button_morethanone():
+@app.route("/click/2/")
+def clicked_button_two():
     print("button x2 clicked")
     session["countPy"] += 2
-    return render_template("index.html")
+    return redirect("/")
 
-@app.route('/destroy_session')
+@app.route("/click/form", methods=["POST"])
+def clicked_send():
+    print("int button clicked")
+    session["countPy"] += int(request.form["click_input"])
+    return redirect("/")
+
+@app.route("/destroy_session")
 def reset_clicked():
     print("reset request sent")
     session["countPy"] = 0
-
-    return render_template("index.html")
+    return redirect("/")
 
 if __name__=="__main__":
     app.run(debug=True)
